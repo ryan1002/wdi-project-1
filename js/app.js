670de4b24c1f688ce0f201109a54cb6fc27d49e0
ryan1002 <ryanmodeste2@gmail.com>
2016-08-20 14:07:51 +0100

    resaved app.js file

--- js/app.js
+++ js/app.js
@@ -4,13 +4,18 @@ var numberOfTrys = 0;
 
 window.onload = init;
 
+
+
 function init(){
 
   var board = document.getElementById('gameBoard');
+  // board.getBoundingClientRect()
   board.addEventListener('click', press);
   board.onmousemove = readMouseMove;
   var innerText = document.getElementById('text');
   // motion dectection
+
+
 
   function readMouseMove(e) {
     var x = e.clientX;
@@ -18,17 +23,28 @@ function init(){
     var coordinates = "Coordinates: (" + x + "," + y + ")";
     document.getElementById("textOne").innerHTML = coordinates;
 
+
   }
+
+  function clearCoor() {
+    document.getElementById("text").innerHTML = "";
+  }
+
+
+// 
+
+
 }
 
-//   function clearCoor() {
-//     document.getElementById("text").innerHTML = "";
-//   }
+function press() {
+  // alert('i was pressed')
+  if(x === 499 && y === 256) {
+    alert('you find me');
+  }
 
+}
 
-// }
-
-
+//499, 256
 
 
 
